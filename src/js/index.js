a3514906bb5fac03b87ba16be57b68ee70934aa7
Celia Martin Herranz <celiamartinherranz11@gmail.com>
2023-02-02 11:21:06 +0100

    add js menu and styles

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,22 @@
 'use strict';
+
+const smallContainer = document.getElementById('small-container');
+const menuContainer = document.getElementById('menu-container');
+const header = document.getElementById('header');
+const linksContainer = document.getElementById('links-container');
 const slidesContainer = document.getElementById('slides-container');
 const slide = document.querySelector('.slide');
 const prevButton = document.getElementById('slide-arrow-prev');
 const nextButton = document.getElementById('slide-arrow-next');
+
+
+smallContainer.addEventListener('click',() => {
+  menuContainer.classList.toggle('close');
+  linksContainer.classList.toggle('close');
+  header.classList.toggle('header-show');
+  smallContainer.classList.toggle('small-container-show');
+});
+
 
 nextButton.addEventListener('click', () => {
   const slideWidth = slide.clientWidth;
